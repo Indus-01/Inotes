5c61d01e90f470de57b6fd53a374cb604d41bcb8
Miral 034 <121128789+Indus-01@users.noreply.github.com>
2023-04-02 09:10:19 +0300

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,15 @@
+function makeTodo(){
+			if (document.querySelector("#t-title").value <= '' & document.querySelector("#t-body").value <= '') {
+				confirm("Enter name and body!!")
+			} else {
+				var tasklist = document.getElementById("tasks");
+			tasklist.innerHTML += `
+				<div class='task'>
+					<h1 class='Note_heading'>${document.querySelector("#t-title").value}</h1>
+					<h1>:</h1>
+					<h1 class='Note_body'>${document.querySelector("#t-body").value}</h1>
+					<button id='del' onclick='var task = document.querySelector(".task"); task.outerHTML = ""'><i class='fas fa-check'></i></button>
+				</div>
+			`;
+		}
+			}
