00c282149c311a42437a79c8be9050344fb631c5
Miral 034 <121128789+Indus-01@users.noreply.github.com>
2023-06-04 18:54:48 +0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -4,9 +4,8 @@ function makeTodo(){
 			} else {
 				var tasklist = document.getElementById("tasks");
 			tasklist.innerHTML += `
-				<div class='task'>
+				<div class='task' style='display: flex; flex-direction: column;'>
 					<h1 class='Note_heading'>${document.querySelector("#t-title").value}</h1>
-					<h1>:</h1>
 					<h1 class='Note_body'>${document.querySelector("#t-body").value}</h1>
 					<button id='del' onclick='var task = document.querySelector(".task"); task.outerHTML = ""'><i class='fas fa-check'></i></button>
 				</div>
