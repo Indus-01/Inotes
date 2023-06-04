56612ca1a6fce6a1098d27fabf1e0c34fa986a16
Miral 034 <121128789+Indus-01@users.noreply.github.com>
2023-06-05 00:43:21 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.querySelector('#button').onclick = function(){
+    var t = document.querySelector('.tasks').innerHTML += 
+    `<div class="task">
+          <h2 id='task-title' class='text'>${document.querySelector('#input').value}</h2>
+          <button class='finished' id='finished'>completed</button>
+        </div>
+    `;
+};
+
+
