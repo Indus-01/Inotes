44ed9c3e8317128a0d33d66ac9f69a109bc1bc92
Miral 034 <121128789+Indus-01@users.noreply.github.com>
2023-06-04 19:06:13 +0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -7,7 +7,7 @@ function makeTodo(){
 				<div class='task' style='display: flex; flex-direction: column;'>
 					<h1 class='Note_heading'>${document.querySelector("#t-title").value}</h1>
 					<h1 class='Note_body'>${document.querySelector("#t-body").value}</h1>
-					<button id='del' onclick='var task = document.querySelector(".task"); task.outerHTML = ""'><i class='fas fa-check'></i></button>
+					<button id='del' style='width: 48px; height: 48px;' onclick='var task = document.querySelector(".task"); task.outerHTML = ""'><i class='fas fa-check'></i></button>
 				</div>
 			`;
 		}
