7a4db7916835dc06c0e2a33b7f4fefd17046b1bb
Miral 034 <121128789+Indus-01@users.noreply.github.com>
2023-06-06 14:58:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ document.querySelector('#button').onclick = function(){
     var t = document.querySelector('.tasks').innerHTML += 
     `<div class="task">
           <h2 id='task-title' class='text'>${document.querySelector('#input').value}</h2>
-          <button class='finished' id='finished'  onclick='this.parentElement.remove()'>completed</button>
+          <button class='finished' id='finished'  onclick='this.parentElement.remove();document.querySelector('#input').value = ""'>completed</button>
         </div>
     `;
-    document.querySelector('#input').value = '';
 };
 
 
